5b716b6faaaaef911a6bb88712d0c73ffa4fd845
CodingToLive <85699360+CodingToLive@users.noreply.github.com>
2026-02-22 12:07:10 +0900

    add `get_all_rates` method to utilize all combinations from PricingConstants in API requests

--- dynamic-pricing/lib/rate_api_client.rb
+++ dynamic-pricing/lib/rate_api_client.rb
@@ -1,5 +1,6 @@
 class RateApiClient
   include HTTParty
+  include PricingConstants
   base_uri ENV.fetch('RATE_API_URL', 'http://localhost:8080')
   headers "Content-Type" => "application/json"
   headers 'token' => ENV.fetch('RATE_API_TOKEN', '04aa6f42aa03f220c2ae9a276cd68c62')
@@ -13,6 +14,13 @@ class RateApiClient
           room: room
         }
       ]
+    }.to_json
+    self.post("/pricing", body: params, timeout: 10)
+  end
+
+  def self.get_all_rates
+    params = {
+      attributes: ALL_COMBINATIONS
     }.to_json
     self.post("/pricing", body: params, timeout: 10)
   end
